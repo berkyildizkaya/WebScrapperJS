a694ca7c60881f5f3343a9a44e14f37a5a1189f1
Berk Yıldızkaya <berk.yildizkaya@royrobson.com>
2023-11-29 13:27:52 +0300

    Create project files

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# WebScrapperJS
+# How to web scraping with Javascript in Amazon
+
+**Requirements**
+1. NodeJS
+2. Cheerio
+3. Amazon Product
+
+# Usage
+## Create Telegram Bot
+1. https://telegram.me/BotFather go to link
+2. write **/start** to chat  and send message
+3. write **/newbot** to chat and send message
+4. give a username but It must end in **bot** keyword
+5. Father bot give you token 
+6. Create group chat in telegram
+7. invite **@getidsbot** to your group
+8. When **@getidsbot** join the group  Bot gives you the group_id
+9. Take this chat id in the message. Chat_id must starts with **-**
+
+## Create .env file
+### Params
+1. BOT_TOKEN
+2. CHAT_ID
+3. PRODUCT_URL
+4. CONTROL_PRICE
+
+## Start
+**npm start**
+# Program  
+
+**Program Basically if you want to buy any product and you don't have a time for always control the product price This is a program that does your job**
+
---
+++ index.js
@@ -0,0 +1,65 @@
+// import fetch from 'node-fetch';
+import * as cheerio from "cheerio";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+const getAmazonProduct = async () => {
+  try {
+    const url = process.env.PRODUCT_URL;
+    const headers = {
+      "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      "Accept-Language": "en-US, en;q=0.5",
+    };
+    const response = await axios.get(url, { headers });
+    var $ = cheerio.load(response.data);
+    const priceElement = $("span.a-price-whole").first();
+    const priceText = priceElement.text().replace(",", "");
+    const sellerElement = $("span.a-size-small.tabular-buybox-text-message").eq(1);
+    const sellerText = sellerElement.text();
+  
+    let SendMessage = "";
+    SendMessage =
+      priceText === "" || sellerText === ""
+        ? "Product does not found or seller does not found"
+        : `Product Price :${priceText} Product Seller : ${sellerText}`;
+    if (parseInt(priceText.replace('.','')) < parseInt(process.env.CONTROL_PRICE)){
+      setOptions(SendMessage);
+    }
+  } catch (error) {
+    setOptions(error);
+    console.log(error);
+  }
+};
+const setOptions = (sendMessage) => {
+  const options = {
+    method: "POST",
+    url: `https://api.telegram.org/${process.env.BOT_TOKEN}/sendMessage`,
+    headers: {
+      accept: "application/json",
+      "User-Agent":
+        "Telegram Bot SDK - (https://github.com/irazasyed/telegram-bot-sdk)",
+      "content-type": "application/json",
+    },
+    data: {
+      text: sendMessage,
+      disable_web_page_preview: false,
+      disable_notification: false,
+      reply_to_message_id: null,
+      chat_id: process.env.chat_id,
+    },
+  };
+  axios(options)
+    .then((response) => {
+      // console.log('Telegram API Response:', response.data);
+    })
+    .catch((error) => {
+      console.error("Telegram API Error:", error.message);
+    });
+};
+
+setInterval(() => {
+  getAmazonProduct();
+}, 1800000);
+
+getAmazonProduct();
